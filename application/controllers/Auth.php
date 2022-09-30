74ccded98a70f10cd4b2cc9de0f4c93f3652ffb0
raupsss <androghanz@gmail.com>
2022-09-30 16:00:13 +0700

    latihan

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -23,6 +23,11 @@ class Auth extends CI_Controller
 
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match',
+            'min_length' => 'Password too short'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
 
         if ($this->form_validation->run() == false) {
@@ -31,7 +36,20 @@ class Auth extends CI_Controller
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'Data berhasil ditambahkan';
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'data_created' => time()
+
+            ];
+
+            $this->db->insert('user', $data);
+            // $this->session->set_flashdata('message', '')
+            redirect('auth');
         }
     }
 }
